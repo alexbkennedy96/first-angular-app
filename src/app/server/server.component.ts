8052e4e4606fe91a5ca40c26641b4e73bef55a97
“alexbkennedy96” <akenne36@uncc.edu>
2019-08-27 11:23:09 -0400

    Initial Commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+// This decorator tells typescript that this is a component
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
